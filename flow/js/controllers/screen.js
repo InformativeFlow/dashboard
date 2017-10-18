534834b6d288b4d640a000fbc4aed5b4f39f508a
alejandroquintero <a.quintero10@uniandes.edu.co>
2017-10-17 23:32:28 -0500

    se conectan servicios con backend

--- flow/js/controllers/screen.js
+++ flow/js/controllers/screen.js
@@ -19,17 +19,18 @@ function screenCtrl($scope, $http, $state, $q, $uibModal) {
     $http.get('https://c354kdhd51.execute-api.us-west-2.amazonaws.com/prod/branches?TableName=branch').then(function (response) {
         $scope.screens = response.data.Items;
         for (var item in $scope.screens) {
-         
+
             if ($scope.screens[item].id["N"] == $scope.branchId) {
+                $scope.branchSelectedName = $scope.screens[item].name["S"];
                 $scope.screensBranch = $scope.screens[item].screens["L"];
-                
+
             }
         }
-       
+
     });
 
     $http.get('https://r4mhv473uk.execute-api.us-west-2.amazonaws.com/prod/dbimages?TableName=image').then(function (response) {
-     $scope.contentImages = response.data.Items;     
+        $scope.contentImages = response.data.Items;
 
     });
     $http.get('https://1y0rxj9ll6.execute-api.us-west-2.amazonaws.com/prod/dbvideos?TableName=video').then(function (response) {
@@ -40,16 +41,40 @@ function screenCtrl($scope, $http, $state, $q, $uibModal) {
 
     $scope.hideMe = function () {
         return $scope.list4.length > 0;
-    }
+    };
 
     $scope.saveContent = function () {
 
         if ($scope.list4.length > 0) {
-            $http.post('api/content/save', [$scope.list4, {"branchId": $scope.branchId}, {"screenId": $scope.screenId}]).then(function (response) {
+            var mapbuilder = {};
+            $scope.listReady = [];
+            for (var item in $scope.list4) {
+                delete $scope.list4[item]["$$hashKey"];
+                mapbuilder["M"] = $scope.list4[item];
+                $scope.listReady.push(mapbuilder);
+            }
+            var idx = $scope.screenId - 1;
+            var params = {
+                "TableName": "branch",
+                "Key": {
+                    "name": {
+                        "S": $scope.branchSelectedName
+                    }
+                },
+                "UpdateExpression": "SET #ri[" + idx + "].content = list_append( #ri[" + idx + "].content,:vals)",
+                "ExpressionAttributeNames": {"#ri": "screens"},
+                "ExpressionAttributeValues": {
+                    ":vals": {"L": $scope.listReady}
+                }
+            };
 
+
+            $http.put('https://c354kdhd51.execute-api.us-west-2.amazonaws.com/prod/branches', params).then(function (response) {
+                console.lod(JSON.stringify(response.data));
             });
+          
         }
-    }
+    };
 
     $scope.deleteContent = function () {
         $scope.list4 = [];
@@ -60,11 +85,11 @@ function screenCtrl($scope, $http, $state, $q, $uibModal) {
     $scope.beforeDrop = function () {
 
         var deferred = $q.defer();
-      //  if (confirm('¿Desea borrar el contenido?')) {
-            $scope.list2 = {};
-            deferred.resolve();
-       // } else {
-         //   deferred.reject();
+        //  if (confirm('¿Desea borrar el contenido?')) {
+        $scope.list2 = {};
+        deferred.resolve();
+        // } else {
+        //   deferred.reject();
         //}
         return deferred.promise;
     };
