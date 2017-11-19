6b012269aff6d6eba1065a4331adc21497a20bf9
Abimelec Cuesta <acuestap@gmail.com>
2017-11-19 03:07:13 -0500

    Ajustes al carrusel de promociones
    - Se asigna de manera dinámica la url de la imagen QR.
    - Se depura el JSON de promociones.

--- flow/js/controllers/display.js
+++ flow/js/controllers/display.js
@@ -109,7 +109,7 @@ function displayCtrl($scope, $http, $state, creds,configService, $interval) {
 
         });
         
-        $scope.myInterval = 5000;
+        $scope.myInterval = 10000;
         $scope.active = 0;    
     
         var showPromotion = function() {
