296cd4d6d5d5b54ed583db6224ad553cd5ad73ad
alejandroquintero <a.quintero10@uniandes.edu.co>
2017-11-19 09:02:10 -0500

    se conecta funcionalidad autenticacion backend

--- flow/js/controllers/login/login.js
+++ flow/js/controllers/login/login.js
@@ -12,11 +12,13 @@ function loginCtrl($scope, $http, $location, $auth,configService) {
         $http.get('https://zvqzxh7ngd.execute-api.us-west-2.amazonaws.com/prod/users?TableName=user').then(function (response) {
          console.log(JSON.stringify(response.data.Items))
             for (var user in response.data.Items)
-             if (response.data.Items[user].name['S'] == $scope.user.user && response.data.Items[user].password['S'] == $scope.user.password )
+             if (response.data.Items[user].name['S'] == $scope.user.user && response.data.Items[user].password['S'] == $scope.user.password ){
                  window.sessionStorage.setItem('user',response.data.Items[user].id['N']);
             $auth.setToken("iflowtoken8650");
             $location.path('/dashboard');
-            
+        }else{$location.path('/login');
+         $scope.error = "invalid user or password";
+            }
         }).catch(function (error) {
             console.log(error);
         })
