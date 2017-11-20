4ec5872d0310fa0afdacbc0393f7a8b1f880e979
Diego F. Ruiz S <yego23@gmail.com>
2017-11-19 20:26:55 -0500

    Ajustes visuales y login

--- flow/js/controllers/main.js
+++ flow/js/controllers/main.js
@@ -1,65 +1,68 @@
 //main.js
 angular
-.module('app')
-.controller('cardChartCtrl2', cardChartCtrl2)
-.controller('loadBranchesCtrl', loadBranchesCtrl);
+        .module('app')
+        .controller('cardChartCtrl2', cardChartCtrl2)
+        .controller('loadBranchesCtrl', loadBranchesCtrl);
 
 cardChartCtrl2.$inject = ['$scope'];
 function cardChartCtrl2($scope) {
 
-  $scope.labels = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio'];
-  $scope.data = [
-    [5, 18, 9, 17, 34, 22, 11]
-  ];
-  $scope.colors = [{
-    backgroundColor: brandInfo,
-    borderColor: 'rgba(255,255,255,.55)',
-  }];
-  
-  $scope.options = {
-    maintainAspectRatio: false,
-    scales: {
-      xAxes: [{
-        gridLines: {
-          color: 'transparent',
-          zeroLineColor: 'transparent'
-        },
-        ticks: {
-          fontSize: 2,
-          fontColor: 'transparent',
-        }
+    $scope.labels = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
+    $scope.data = [
+        [5, 18, 9, 17, 34, 22, 11]
+    ];
+    $scope.colors = [{
+            backgroundColor: brandInfo,
+            borderColor: 'rgba(255,255,255,.55)',
+        }];
 
-      }],
-      yAxes: [{
-        display: false,
-        ticks: {
-          display: false,
-          min: Math.min.apply(Math, $scope.data[0]) - 5,
-          max: Math.max.apply(Math, $scope.data[0]) + 5
+    $scope.options = {
+        maintainAspectRatio: false,
+        scales: {
+            xAxes: [{
+                    gridLines: {
+                        color: 'transparent',
+                        zeroLineColor: 'transparent'
+                    },
+                    ticks: {
+                        fontSize: 2,
+                        fontColor: 'transparent',
+                    }
+
+                }],
+            yAxes: [{
+                    display: false,
+                    ticks: {
+                        display: false,
+                        min: Math.min.apply(Math, $scope.data[0]) - 5,
+                        max: Math.max.apply(Math, $scope.data[0]) + 5
+                    }
+                }],
+        },
+        elements: {
+            line: {
+                tension: 0.00001,
+                borderWidth: 1
+            },
+            point: {
+                radius: 4,
+                hitRadius: 10,
+                hoverRadius: 4,
+            }
         }
-      }],
-    },
-    elements: {
-      line: {
-        tension: 0.00001,
-        borderWidth: 1
-      },
-      point: {
-        radius: 4,
-        hitRadius: 10,
-        hoverRadius: 4,
-      }
     }
-  }
 }
 
-loadBranchesCtrl.$inject = ['$scope', '$http','configService'];
-function loadBranchesCtrl($scope, $http,configService) {
+loadBranchesCtrl.$inject = ['$scope', '$http', 'configService'];
+function loadBranchesCtrl($scope, $http, configService) {
 
     $scope.branches = [];
     $http.get('https://c354kdhd51.execute-api.us-west-2.amazonaws.com/prod/branches?TableName=branch', configService.getConfig()).then(function (response) {
 
-        $scope.branches = response.data.Items;
+        for (var item in response.data.Items) {
+            if (response.data.Items[item].user['S'] === window.sessionStorage.getItem('user').toString())
+                $scope.branches.push(response.data.Items[item]);
+        }
 
     });
 
